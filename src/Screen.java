7c5e9c26d20228a8c5c86a48c434776a698dee06
DESKTOP-7F19R05\Hao Huynh <hao152903@gmail.com>
2021-02-26 23:20:15 +0700

    no message

--- src/Screen.java
+++ src/Screen.java
@@ -3,6 +3,7 @@ import libs.*;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
@@ -22,19 +23,28 @@ public class Screen extends JFrame {
     private JButton btn_Export;
     private JPanel pannel_encrypt_1;
     private JTextField txt_Filepath;
-    private JTextField textField2;
+    private JTextField txt_encrypt_file;
     private JButton btn_Select;
+    private JTextField txt_Filepath_2;
+    private JButton btn_Select_2;
+    private JTextField txt_decrypt_file;
+    private JButton btn_Export_2;
+    private JButton btn_Decrypt;
     private BigInteger public_key = new BigInteger("0");
     private BigInteger private_key = new BigInteger("0");
     private GenerateKeys g;
-    private String filepath;
+    private String filepath = new String("");
     private String encrypt_file;
     private String decrypt_file;
-    private String filename;
+    private String filename = new String("");
+    private String filepath_2 = new String("");
+    private String filename_2 = new String("");
+
+    private boolean flag = false;
 
     public Screen() {
-        btn_showpass.setIcon(new ImageIcon(getClass().getResource("/media/icons8-show-password-50.png")));
 
+        btn_showpass.setIcon(new ImageIcon(getClass().getResource("/media/icons8-show-password-50.png")));
         //Event when we click the button to make 2 random numbers - p and q
         btn_Random.addActionListener(new ActionListener() {
             @Override
@@ -66,14 +76,36 @@ public class Screen extends JFrame {
         btn_showpass.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                txt_pass.setText(private_key+"");;
+                if(flag == false) {
+                    txt_pass.setText(private_key+"");
+                    flag = true;
+                } else {
+                    txt_pass.setText("****");
+                    flag = false;
+                }
             }
         });
 
         //Event when we click the button to select file
+        btn_Select_2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                txt_Filepath_2.setText("");
+                JFileChooser file = new JFileChooser();
+                file.setMultiSelectionEnabled(true);
+                file.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                file.setFileHidingEnabled(false);
+                if (file.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                    java.io.File f = file.getSelectedFile();
+                    filepath_2 = f.getPath();
+                    txt_Filepath_2.setText(f.getPath());
+                }
+            }});
+
         btn_Select.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                txt_Filepath.setText("");
                 JFileChooser file = new JFileChooser();
                 file.setMultiSelectionEnabled(true);
                 file.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
@@ -87,15 +119,40 @@ public class Screen extends JFrame {
         });
 
         //Event when we click the button to export the result
+
+        btn_Export_2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BufferedWriter writer = null;
+                try {
+                    writer = new BufferedWriter(new FileWriter("D:\\RSA_Project\\src\\result\\Decrypt.txt")); //Your folder's path that you wanna save
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                try {
+                    writer.write(decrypt_file);
+                    writer.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+            }
+        });
+
         btn_Export.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                filename = Functions.readFileAsString(filepath);
-                String encrypt = g.encrypt(filename);
-                String decrypt = g.decrypt(encrypt);
-                if(filename.equals(decrypt)) {
-                    System.out.println("true");
-                } else System.out.println("false");
+                BufferedWriter writer = null;
+                try {
+                    writer = new BufferedWriter(new FileWriter("D:\\RSA_Project\\src\\result\\Encrypt.txt")); //Your folder's path that you wanna save
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
+                try {
+                    writer.write(encrypt_file);
+                    writer.close();
+                } catch (IOException ioException) {
+                    ioException.printStackTrace();
+                }
             }
         });
 
@@ -103,12 +160,30 @@ public class Screen extends JFrame {
         btn_Sign.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if("".equals(filename)) {
+                if("".equals(filepath)) {
                     JOptionPane.showMessageDialog(null, "Chọn file trước khi mã hóa", "Thông báo", JOptionPane.ERROR_MESSAGE);
                 }
                 else {
-                    String filename = Functions.readFileAsString(filepath);
-                    String encrypt = g.encrypt(filename);
+                    filename = Functions.readFileAsString(filepath);
+                    encrypt_file = g.encrypt(filename);
+                    System.out.println(encrypt_file);
+                    txt_encrypt_file.setText(encrypt_file);
+                }
+            }
+        });
+
+        //Event when we click the button to decrypt text
+        btn_Decrypt.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if("".equals(filepath_2)) {
+                    JOptionPane.showMessageDialog(null, "Chọn file trước khi xác thực", "Thông báo", JOptionPane.ERROR_MESSAGE);
+                }
+                else {
+                    filename_2 = Functions.readFileAsString(filepath_2);
+                    decrypt_file = g.decrypt(filename_2);
+                    System.out.println(decrypt_file);
+                    txt_decrypt_file.setText(decrypt_file);
                 }
             }
         });
